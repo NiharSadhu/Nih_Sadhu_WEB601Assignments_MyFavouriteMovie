46cf170c1271db52a0cdb10c342536dfe2bb2af7
NiharSadhu <113557223+NiharSadhu@users.noreply.github.com>
2023-07-19 05:22:09 -0400

    update

---
+++ Nih_Sadhu_MyFavouriteMovie/src/app/services/update.service.ts
@@ -0,0 +1,21 @@
+// // update.service.ts
+// import { Injectable } from '@angular/core';
+// // import { SwUpdate } from '@angular/service-worker';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UpdateService {
+//   constructor(private swUpdate: SwUpdate) {
+//     if (this.swUpdate.isEnabled) {
+//       this.swUpdate.available.subscribe(event => {
+//         this.showUpdateSnackBar();
+//       });
+//     }
+//   }
+
+//   private showUpdateSnackBar() {
+//     // Display a snackbar with an action button
+//     // When the user clicks the action button, activate the update and reload the page
+//   }
+// }
